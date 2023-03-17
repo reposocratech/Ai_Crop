f750aa2b012b02ba049325968a9c2db0e57c4741
Gabriel Delgado <gadetru@gmail.com>
2023-03-17 13:03:36 +0100

    reparación greenhouse

--- server/routes/clientRoutes/greenHouse.js
+++ server/routes/clientRoutes/greenHouse.js
@@ -4,13 +4,20 @@ var express = require('express');
 const GreenHouseController = require('../../controllers/clientControllers/greenhouseControllers');
 var router = express.Router();
 
-//---.trae la info de todos los invernaderos
-//localhost:4000/greenHouse/getAllGreenHouses/:user_owner_id
-router.get("/getAllGreenHouses/:user_owner_id", GreenHouseController.getAllGreenHouses);
+//1. create greenhouse
+// localhost:4000/greenhouse/createGreenhouse
+router.get('/createGreenhouse', GreenhouseController.createGreenhouse);
+// TO DO:
+// CAMBIAR metodo a POST
 
 
-/* //---trae todos los cultivos de un invernadero, activo e inactivo
-//localhost:4000/greenHouse/:greenhouse_id/allCrops
+//2. edit greenhouse
+// localhost:4000/greenhouse/createGreenhouse
+router.post('/editGreenhouse/:greenhouse_id', GreenhouseController.editGreenhouse);
+// TO DO: TESTEAR!!!
+
+
+
 
 router.get("/:greenhouse_id/allCrops", GreenHouseController.getAllCrops); */
 
