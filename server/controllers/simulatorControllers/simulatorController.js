95628c17fed419c3ec270e3189fba2996d34b72d
Javier Morera <javimorera90@hotmail.com>
2023-03-17 10:02:20 +0100

    controlador simulator

--- server/controllers/simulatorControllers/simulatorController.js
+++ server/controllers/simulatorControllers/simulatorController.js
@@ -5,17 +5,17 @@ class SimulatorController {
    // localhost:4000/simulator
    simulateData = (req, res) => {
 
-      let {greenhouse_id, temperatura, co2, humedad, luz_solar} = req.body.datosForm;
+      let {greenhouse_id, temperatura} = req.body.datosForm;
 
       let sql = `INSERT INTO measure (greenhouse_id, measurement_type_id, measure_value) VALUES 
       (${greenhouse_id}, 1, ${temperatura})`;
 
-      req.body.datosForm.co2 && (sql += ` ,(${greenhouse_id}, 2, ${co2})`);
-      req.body.datosForm.humedad && (sql += ` ,(${greenhouse_id}, 3, ${humedad})`);
-      req.body.datosForm.luz_solar && (sql += ` ,(${greenhouse_id}, 4, ${luz_solar})`);
-      req.body.datosForm.ph && (sql += ` ,(${greenhouse_id}, 5, ${ph})`);
-      req.body.datosForm.ce && (sql += ` ,(${greenhouse_id}, 6, ${ce})`);
-      req.body.datosForm.humedad_hoja && (sql += ` ,(${greenhouse_id}, 7, ${humedad_hoja})`);
+      req.body.datosForm.co2 && (sql += `, (${greenhouse_id}, 2, ${req.body.datosForm.co2})`);
+      req.body.datosForm.humedad && (sql += `, (${greenhouse_id}, 3, ${req.body.datosForm.humedad})`);
+      req.body.datosForm.luz_solar && (sql += `, (${greenhouse_id}, 4, ${req.body.datosForm.luz_solar})`);
+      req.body.datosForm.ph && (sql += `, (${greenhouse_id}, 5, ${req.body.datosForm.ph})`);
+      req.body.datosForm.ce && (sql += `, (${greenhouse_id}, 6, ${req.body.datosForm.ce})`);
+      req.body.datosForm.humedad_hoja && (sql += `, (${greenhouse_id}, 7, ${req.body.datosForm.humedad_hoja})`);
 
       connection.query(sql, (error, result) => {
          error
--- server/routes/simulatorRoutes/simulatorRouter.js
+++ server/routes/simulatorRoutes/simulatorRouter.js
@@ -1,9 +1,10 @@
 var express = require('express');
-const simulatorController = require('../../controllers/simulatorControllers/simulatorController');
+const SimulatorController = require('../../controllers/simulatorControllers/simulatorController');
+
 var router = express.Router();
 
 
 // localhost:4000/simulator
-router.post('/', simulatorController.simulateData);
+router.post('/', SimulatorController.simulateData);
 
 module.exports = router;
