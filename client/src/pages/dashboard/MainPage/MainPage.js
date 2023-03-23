0a0ec8dfdd2e4e5c34e7cd6826c72e39e23a5601
Nazareth Vigueras <nnviguerasc@gmail.com>
2023-03-23 09:05:12 +0100

    cards greenhouse

--- client/src/pages/dashboard/MainPage/MainPage.js
+++ client/src/pages/dashboard/MainPage/MainPage.js
@@ -11,7 +11,7 @@ export const MainPage = () => {
   return (
     <Container fluid className='p-0'>
         <Row className='contNav_pPal'>
-            <Col className='col-12 col-xl-3 p-0 navvv'>
+            <Col className='col-12 col-xl-3 p-0'>
               {/* {user_type == 1} */}
                 <NavLateral/>
             </Col>
